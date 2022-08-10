8b95de7a811656c4ff954a7a44e87d4a3ea274a2
Andermen153 <duxa.ru14@gmail.com>
2022-08-10 12:51:54 +0500

    Version2

--- SinitsinPackage/Schemas/AndreySchema86ae7ea1Detail/AndreySchema86ae7ea1Detail.js
+++
@@ -1,8 +0,0 @@
-define("AndreySchema86ae7ea1Detail", [], function() {
-	return {
-		entitySchemaName: "AndreyDetailAppealTickets",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
--- SinitsinPackage/Schemas/AndreyServicesbf76366eSection/AndreyServicesbf76366eSection.js
+++
@@ -1,8 +0,0 @@
-define("AndreyServicesbf76366eSection", [], function() {
-	return {
-		entitySchemaName: "AndreyServices",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
